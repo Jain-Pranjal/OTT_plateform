df24397611253d102c4417d71b7bc2687b9ce175
Pranjal Jain <pranjalworkon@gmail.com>
2024-06-01 15:11:05 +0530

    gathering the form data

--- components/ContactCard.jsx
+++ components/ContactCard.jsx
@@ -1,7 +1,37 @@
 "use client"
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import Link from 'next/link'
+import tailwindConfig from '@/tailwind.config'
 const ContactCard = () => {
+    const [formdata,setFormData]=useState({
+        username:'',
+        email:'',
+        subject:'',
+        message:''
+    })
+
+    const handleChange = (e) => {
+      setFormData({...formdata,[e.target.name]:[e.target.value]})
+    }
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+        // getting the data from the form
+      
+        // console.log(formname,formemail,formsubject,formmessage)
+
+        const submitData = await fetch('/api/formdata', {
+            method: 'POST',
+            body: JSON.stringify({ formdata }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const response = await submitData.json()
+        console.log(response)
+
+        
+    }
   return (
     <div>
       <div className="grid sm:grid-cols-2 items-center gap-16 my-6 mx-auto max-w-4xl bg-white text-[#333] font-[sans-serif]">
@@ -16,8 +46,7 @@ const ContactCard = () => {
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" fill='#007bff'
                                     viewBox="0 0 479.058 479.058">
                                     <path
-                                        d="M434.146 59.882H44.912C20.146 59.882 0 80.028 0 104.794v269.47c0 24.766 20.146 44.912 44.912 44.912h389.234c24.766 0 44.912-20.146 44.912-44.912v-269.47c0-24.766-20.146-44.912-44.912-44.912zm0 29.941c2.034 0 3.969.422 5.738 1.159L239.529 264.631 39.173 90.982a14.902 14.902 0 0 1 5.738-1.159zm0 299.411H44.912c-8.26 0-14.971-6.71-14.971-14.971V122.615l199.778 173.141c2.822 2.441 6.316 3.655 9.81 3.655s6.988-1.213 9.81-3.655l199.778-173.141v251.649c-.001 8.26-6.711 14.97-14.971 14.97z"
-                                        data-original="#000000" />
+                                        d="M434.146 59.882H44.912C20.146 59.882 0 80.028 0 104.794v269.47c0 24.766 20.146 44.912 44.912 44.912h389.234c24.766 0 44.912-20.146 44.912-44.912v-269.47c0-24.766-20.146-44.912-44.912-44.912zm0 29.941c2.034 0 3.969.422 5.738 1.159L239.529 264.631 39.173 90.982a14.902 14.902 0 0 1 5.738-1.159zm0 299.411H44.912c-8.26 0-14.971-6.71-14.971-14.971V122.615l199.778 173.141c2.822 2.441 6.316 3.655 9.81 3.655s6.988-1.213 9.81-3.655l199.778-173.141v251.649c-.001 8.26-6.711 14.97-14.971 14.97z"data-original="#000000" />
                                 </svg>
                             </div>
                             <Link href="mailto:pranjalworkon@gmail.com" className="text-[#007bff] text-sm ml-3">
@@ -64,15 +93,15 @@ const ContactCard = () => {
                 </div>
             </div>
             <form className="ml-auo space-y-4">
-                <input type='text' placeholder='Name' required
-                    className="w-full rounded-md py-3 px-4 bg-gray-100 text-sm outline-[#007bff]" />
-                <input type='email' placeholder='Email' required
-                    className="w-full rounded-md py-3 px-4 bg-gray-100 text-sm outline-[#007bff]" />
-                <input type='text' placeholder='Subject'
-                    className="w-full rounded-md py-3 px-4 bg-gray-100 text-sm outline-[#007bff]" />
-                <textarea placeholder='Message' rows="6"
-                    className="w-full rounded-md px-4 bg-gray-100 text-sm pt-3 outline-[#007bff]"></textarea>
-                <button type='submit' onSubmit={() => alert('Message Sent!')}
+                <input type='text' placeholder='Name' required name='username'
+                    className="w-full rounded-md py-3 px-4 bg-gray-100 text-sm outline-[#007bff]"  value={formdata.username} onChange={handleChange}/>
+                <input type='email' placeholder='Email' required name='email'
+                    className="w-full rounded-md py-3 px-4 bg-gray-100 text-sm outline-[#007bff]"  value={formdata.email} onChange={handleChange}/>
+                <input type='text' placeholder='Subject' required name='subject'
+                    className="w-full rounded-md py-3 px-4 bg-gray-100 text-sm outline-[#007bff]"  value={formdata.subject} onChange={handleChange}/>
+                <textarea placeholder='Message' rows="6" required name='message'
+                    className="w-full rounded-md px-4 bg-gray-100 text-sm pt-3 outline-[#007bff]" value={formdata.message} onChange={handleChange}></textarea>
+                <button type='submit' onSubmit={handleSubmit}
                     className="text-white bg-[#007bff] hover:bg-blue-600 font-semibold rounded-md text-sm px-4 py-3 w-full">Send</button>
             </form>
         </div>
