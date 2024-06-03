7790aae471a3927c411c95fb23995b51f9123837
Pranjal Jain <pranjalworkon@gmail.com>
2024-06-03 16:51:31 +0530

    toggling of the menu

--- app/scripts/toggling.js
+++ app/scripts/toggling.js
@@ -1,19 +1,20 @@
 
-  
-  window.onload = function() {
-    const toggleOpen = document.getElementById('toggleOpen');
-    const toggleClose = document.getElementById('toggleClose');
-    const collapseMenu = document.getElementById('collapseMenu');
-  
-    function handleClick() {
-        if (collapseMenu.style.display === 'block') {
-            collapseMenu.style.display = 'none';
-          } else {
-            collapseMenu.style.display = 'block';
-          }
+window.onload = function() {
+  const toggleOpen = document.getElementById('toggleOpen');
+  const toggleClose = document.getElementById('toggleClose');
+  const collapseMenu = document.getElementById('collapseMenu');
+
+  function handleClick() {
+   
+if (collapseMenu.style.display === 'block') {
+      collapseMenu.style.display = 'none';
+    } else {
+      collapseMenu.style.display = 'block';
     }
-  
-    toggleOpen.addEventListener('click', handleClick);
-    toggleClose.addEventListener('click', handleClick);
-  };
-  
+  }
+
+  toggleOpen.addEventListener('click', handleClick);
+  toggleClose.addEventListener('click', handleClick);
+};
+
+
